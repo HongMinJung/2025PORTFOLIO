e13c17bc21523e6b01304fb1660483b248fab1ba
HongMinJung <hminjung99@gmail.com>
2025-06-08 19:12:28 +0900

    배포 오류 해결

--- src/components/ui/animated-element.tsx
+++ src/components/ui/animated-element.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { motion, HTMLMotionProps } from "framer-motion";
-import { fadeIn } from "@/hooks/use-animations";
+import { motion, HTMLMotionProps, Variants } from "framer-motion";
 
 interface AnimatedElementProps extends HTMLMotionProps<"div"> {
   children: React.ReactNode;
@@ -15,9 +14,28 @@ export function AnimatedElement({
   delay = 0.2,
   ...props
 }: AnimatedElementProps) {
+  const variants: Variants = {
+    hidden: {
+      y: direction === "up" ? 80 : direction === "down" ? -80 : 0,
+      x: direction === "left" ? 80 : direction === "right" ? -80 : 0,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "tween",
+        duration: 1.2,
+        delay: delay,
+        ease: [0.25, 0.25, 0.25, 0.75],
+      },
+    },
+  };
+
   return (
     <motion.div
-      variants={fadeIn(direction, delay)}
+      variants={variants}
       initial="hidden"
       whileInView="show"
       viewport={{ once: true, amount: 0.25 }}
